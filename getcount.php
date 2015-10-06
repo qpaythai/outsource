07dbf1c4f06a903cd019ed8442f31c1cafb20ae9
Hitesh Solanki <hiteshhilok@gmail.com>
2015-10-06 11:52:12 +0530

    sample table structure has been added for log file, if we add latitude and longitude so we can filter assoicated log according veiwable area and other filter, as of now GPS are in detail column which will not allow us to filter data.

--- getcount.php
+++ getcount.php
@@ -1,6 +1,6 @@
 <?
 include("../main_include.php");
-$resultset = ROOT::query("select count(id) as c from log group by id" );
+$resultset = ROOT::query("select count(id) as c from log" );
 var_dump($resultset);
 
 ?>
---
+++ log-sample.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2015 at 08:19 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` double NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `detail` text NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  `create_date` int(10) NOT NULL,
+  `ip` varchar(15) NOT NULL,
+  `passcode` varchar(20) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
