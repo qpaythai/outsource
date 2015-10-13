1307161f64ce519fb2e76d032bd489649efc70db
mr-monai <monai@monais-iMac.local>
2015-10-13 17:41:20 +0700

    example of mysql framwork

--- getcount.php
+++ getcount.php
@@ -5,8 +5,9 @@ $arr = array();
 switch($action){
 	case 'getcount':
 	$resultset = ROOT::query("select count(id) as c from log" );
-	$row = $resultset->fetch_object();
-	$arr['count'] = $row['c'];
+	$row = mysqli_fetch_row($resultset);
+	//$resultset->fetch_object();
+	$arr['count'] = $row[0];
 	break;
 	case 'mapviewcount':
 	extract($_GET);
@@ -14,12 +15,14 @@ switch($action){
 	$e=104.74223327636719;
 	$s=10.449382871076676;
 	$w=93.20658874511719;
-echo 	$sql = "select count(id) as c from log where ( gpslat <= ".$n." and gpslat >= ".$s." and gpslong >= ".$w." and gpslong <= ".$e." ) ";
-	$resultset = ROOT::query($sql );
-	var_dump($resultset);
-	$row = $resultset->fetch_object();
-	var_dump($row);
-	$arr['count'] = $row['c'];
+//echo 	$sql = "select count(id) as c from log where ( gpslat <= ".$n." and gpslat >= ".$s." and gpslong >= ".$w." and gpslong <= ".$e." ) ";
+	$logtransaction = new log();
+	$logtransaction->gpslat = "!# between $n and $s #!";
+	$logtransaction->gpslong = "!# between $w and $e #!";
+	$logtransaction->loadmany();
+	
+	$logtransaction->track();
+	
 	break;
 }
 
