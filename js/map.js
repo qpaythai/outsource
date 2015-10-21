a8ea2aec800e8c2b3e60102b3c014223b7427d41
Elvithomas4 <elvisthomas4@gmail.com>
2015-10-21 12:30:13 +0530

    map.js has been updated to clear cluster master on change of zoom .

--- js/map.js
+++ js/map.js
@@ -28,54 +28,6 @@ var styles = [[{
         anchor: [32, 0],
         textColor: '#ffffff',
         textSize: 12
-      }], [{
-        url: 'images/conv30.png',
-        height: 27,
-        width: 30,
-        anchor: [3, 0],
-        textColor: '#ff00ff',
-        textSize: 10
-      }, {
-        url: 'images/conv40.png',
-        height: 36,
-        width: 40,
-        anchor: [6, 0],
-        textColor: '#ff0000',
-        textSize: 11
-      }, {
-        url: 'images/conv50.png',
-        width: 50,
-        height: 45,
-        anchor: [8, 0],
-        textSize: 12
-      }], [{
-        url: 'images/heart30.png',
-        height: 26,
-        width: 30,
-        anchor: [4, 0],
-        textColor: '#ff00ff',
-        textSize: 10
-      }, {
-        url: 'images/heart40.png',
-        height: 35,
-        width: 40,
-        anchor: [8, 0],
-        textColor: '#ff0000',
-        textSize: 11
-      }, {
-        url: 'images/heart50.png',
-        width: 50,
-        height: 44,
-        anchor: [12, 0],
-        textSize: 12
-      }], [{
-        url: 'images/pin.png',
-        height: 48,
-        width: 30,
-        anchor: [-18, 0],
-        textColor: '#ffffff',
-        textSize: 10,
-        iconAnchor: [15, 48]
       }]];
  var imageUrl = 'https://storage.googleapis.com/support-kms-prod/SNP_2752125_en_v0';
 console.log(imageUrl);
@@ -154,11 +106,13 @@ getData = function(){
 				markers[i].setVisible(false);
 			}
 		}
-
+		if(markerClusterer)
+			markerClusterer.clearMarkes();
         markerClusterer = new MarkerClusterer(map, mrk, {
           maxZoom: 16,
           gridSize: 50,
           styles: styles[-1]
         });
-		console.log('total visible marker',markerClusterer,c);
+		mrk=[];
+//		console.log('total visible marker',markerClusterer,c);
 	}
