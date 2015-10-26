883a2e4e8c6c4b86409cfc12d4d29a4a68e7fa5c
mr-monai <monai@monais-iMac.local>
2015-10-26 15:41:07 +0700

    update getcount.php

--- getcount.php
+++ getcount.php
@@ -1,8 +1,6 @@
 <?
 include("../main_include.php");
 $action = $_GET['action'];
-$arr = array();
-
 extract($_REQUEST);
 	$n=isset($_REQUEST['n']) && $_REQUEST['n']!=''? $_REQUEST['n'] : 10.449382871076676;
 	$e=isset($_REQUEST['e']) && $_REQUEST['e']!=''? $_REQUEST['e'] : 104.74223327636719;
@@ -38,14 +36,15 @@ switch($action){
 	if(is_numeric($_POST['page'])) $page = $_POST['page'];
 	else $page = 1;
 	if(is_numeric($_POST['limit'])) $limit =$_POST['limit'];
-	else $limit = 10;
+	else $limit = 1000;
 	
 	$logtransaction->id = "!# NOT IN ($ids) #!";
 	$logtransaction->loadmany(' order by id desc',$limit,$page);
-	
+
 	$arr->id = $logtransaction->id;
 	$arr->lat = $logtransaction->gpslat;
 	$arr->long = $logtransaction->gpslong;	
+
 	break;
 }
 
